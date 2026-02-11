62b4f0e8ea8d2b936933965c6c2f80f7f2561e1b
Wayne Bui <damienbft@gmail.com>
2026-02-11 20:18:27 +0700

    4.2

---
+++ part4/blog-list/app.js
@@ -0,0 +1,23 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blog')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+
+mongoose.set('strictQuery', false)
+logger.info('connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('Connected to MongoDB')
+    })
+    .catch(error => {
+        logger.error('Error connecting to MongoDB', error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
