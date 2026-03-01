2a9757cc6f74c80b176077f3aa5e5fac5b3b673d
Wayne PC <damienbft@gmail.com>
2026-03-01 20:43:01 +0700

    4.23*

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -8,7 +8,7 @@ const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
-
+const morgan = require('morgan')
 
 mongoose.set('strictQuery', false)
 mongoose.connect(config.MONGODB_URI)
@@ -21,6 +21,10 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+morgan.token('body', (request) => {
+    return JSON.stringify(request.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(middleware.tokenExtractor)
 app.use('/api/blogs', middleware.userExtractor, blogsRouter)
 app.use('/api/users', userRouter)
