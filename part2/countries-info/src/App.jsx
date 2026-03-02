01ded3e13b9c8a684ed63f1e3db74230f5c50bc3
Wayne PC <damienbft@gmail.com>
2026-03-02 15:23:28 +0700

    feat: Implement initial country information application with search, country details, and weather display for capitals.

--- part2/countries-info/src/App.jsx
+++ part2/countries-info/src/App.jsx
@@ -1,21 +1,21 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import getAllCountries from '../src/countriesService/countries'
+import getAllCountries from './countriesService/countries'
 import axios from 'axios'
 const api_key = import.meta.env.VITE_Weather_API
 const CountryDetail = ({ country }) => {
   const [weather, setWeather] = useState(null)
-  const capital = country.capital[0]
+  const capital = country.capital ? country.capital[0] : 'N/A'
   const lat = country.capitalInfo?.latlng?.[0]
   const lon = country.capitalInfo?.latlng?.[1]
 
   useEffect(() => {
-    if (api_key) {
-      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`)
-      .then(response => setWeather(response.data))
-      .catch(error => {
-        console.log('Weather error', error)
-      })
+    if (api_key && lat !== undefined && lon !== undefined) {
+      axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`)
+        .then(response => setWeather(response.data))
+        .catch(error => {
+          console.log('Weather error', error)
+        })
     }
   }, [lat, lon])
   console.log(weather)
@@ -28,7 +28,7 @@ const CountryDetail = ({ country }) => {
 
       <h3>languages:</h3>
       <ul>
-        {Object.values(country.languages).map(lang => 
+        {Object.values(country.languages || {}).map(lang =>
           <li key={lang}>{lang}</li>
         )}
       </ul>
@@ -37,11 +37,11 @@ const CountryDetail = ({ country }) => {
         src={country.flags.png}
         alt={`Flag of ${country.name.common}`}
         width="150"
-      /> 
+      />
       {weather ? (
         <div>
           <h2>Weather in {capital}</h2>
-          <p>temperature {weather.main.temp} Celcius</p>
+          <p>temperature {weather.main.temp} Celsius</p>
           <img
             src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
             alt='weather icon'
@@ -61,14 +61,14 @@ const App = () => {
 
   useEffect(() => {
     getAllCountries()
-    .then(init => setCountries(init))
+      .then(init => setCountries(init))
   }, [])
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value)
   }
 
-  const countriesToShow = countries.filter(c => 
+  const countriesToShow = countries.filter(c =>
     c.name.common.toLowerCase().includes(filter.toLowerCase())
   )
   return (
@@ -78,7 +78,7 @@ const App = () => {
       </div>
 
       {countriesToShow.length > 10 ? (
-        <p>Too many mathces, specify another filter</p>
+        <p>Too many matches, specify another filter</p>
       ) : countriesToShow.length === 1 ? (
         <CountryDetail country={countriesToShow[0]} />
       ) : (
