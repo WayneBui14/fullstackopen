5fe7c345ed9a35c832acbbcc4139c7d4a0d51d9d
Wayne Bui <damienbft@gmail.com>
2026-04-01 03:09:29 +0700

    Init bloglist-backend-p7

---
+++ part7/bloglist-backend-p7/app.js
@@ -0,0 +1,40 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blog')
+const userRouter = require('./controllers/user')
+const loginRouter = require('./controllers/login')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
+const morgan = require('morgan')
+
+mongoose.set('strictQuery', false) // Khi strictQuery: true (mặc định), Mongoose sẽ tự động loại bỏ các field không có trong Schema khi query
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('Connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('Error connecting to MongoDB', error.message)
+  })
+
+app.use(cors()) // Middleware để cho phép request từ các domain khác
+app.use(express.json()) // Middleware để parse JSON request sang JavaScript object
+morgan.token('body', (request) => {
+  return JSON.stringify(request.body)
+}) // Tạo token để log body request
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+) // Log request ra console
+app.use(middleware.tokenExtractor) // Middleware để lấy token từ header authorization
+app.use('/api/blogs', middleware.userExtractor, blogsRouter) // Nhét userExtractor vào trước blogsRouter để lấy user từ token, middleware này chỉ chạy cho mỗi route api/blogs
+app.use('/api/users', userRouter) // Router lấy danh sách user
+app.use('/api/login', loginRouter) // Router đăng nhập
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/tesing')
+  app.use('/api/testing', testingRouter)
+}
+app.use(middleware.errorHandler) // Middleware xử lý lỗi
+module.exports = app // Xuất app ra ngoài
---
+++ part7/bloglist-backend-p7/controllers/login.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const loginRouter = require('express').Router()
+const User = require('../models/user')
+
+loginRouter.post('/', async (request, response) => {
+  // Lấy body từ request của user gửi lên gán vào biến username và password
+  const { username, password } = request.body
+  // Tìm user trong database theo username của request body
+  const user = await User.findOne({ username })
+  // So sánh password hash của user với password hash của request body
+  const passwordCorrect =
+    user === null ? false : await bcrypt.compare(password, user.passwordHash)
+  // Nếu không tìm thấy user hoặc password không đúng, trả về 401
+  if (!(passwordCorrect && user)) {
+    return response.status(401).json({ error: 'invalid username or password' })
+  }
+  // Tạo userForToken
+  const userForToken = {
+    username: user.username,
+    id: user._id
+  }
+  // Tạo token
+  const token = jwt.sign(userForToken, process.env.SECRET)
+  // Trả về token, username và id
+  response
+    .status(200)
+    .json({ token, username: user.username, name: user.name, id: user._id })
+})
+
+module.exports = loginRouter // Xuất loginRouter ra để dùng ở app.js
